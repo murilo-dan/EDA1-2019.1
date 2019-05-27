2cad2e0b3c4013262dcae9ad0c4406372ed3a47f
Murilo Loiola <messhman.dmc@gmail.com>
2019-05-27 11:15:17 -0300

    Trabalho finalizado.
    Co-authored-by: Gabriel Alves <gabrielhussein83@gmail.com>

--- Project_03/main.c
+++ Project_03/main.c
@@ -1,33 +1,71 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <stdbool.h>
+/*
+Estrutura de Dados 1 - Turma A/2019.1
+Murilo Loiola Dantas - 17/0163571
+Gabriel Alves Hussein - 17/0103200
+*/
 
-typedef struct Contato
+//Bicliotecas utilizadas.
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+//Definição da struct Contato que representa cada nódulo da lista.
+struct Contato
 {
     char nome[102];
     char telefone[12];
     char endereco[102];
     unsigned int cep;
     char nascimento[13];
-    struct Contato *next;
-    struct Contato *prev;
-} Contato;
+    struct Contato *next, *prev;
+};
 
-void inserirRegistro(Contato **);
-void removerRegistrosEspecificos();
-void imprimeRegistrosEspecificos();
-void imprimeRegistrosAlfabeticamente(Contato *);
-void insertionSort(Contato **);
-void sortedInsert(Contato **, Contato *);
-void atualizarTxt(Contato *);
-void push(Contato **, char[], char[], char[], unsigned int, char[], Contato *, Contato *);
+//Declaração do protótipo das funções utilizadas.
+void insertionSort(struct Contato **);
+void imprimirLista(struct Contato *);
+void inserirLista(struct Contato **, char[], char[], char[], unsigned int, char[]);
+void imprimirRegistrosEspecificos(struct Contato *);
+void removerRegistrosEspecificos(struct Contato **);
+void freeRegistros(struct Contato **);
+void atualizarTxt(struct Contato *);
+void atualizarLista(struct Contato **);
+struct Contato *inserirRegistro();
 
-int main()
+//Início da função principal.
+int main(int argc, char** argv)
 {
-    Contato *head = NULL;
+    //Definição da head(começo da lista), auxiliar e arquivo.
+    struct Contato *head = NULL;
+    struct Contato *aux;
+    FILE *Agenda;
     int escolha = 0;
+
+    Agenda = fopen("contatos.txt", "r");
+    //Caso arquivo não exista, cria o arquivo.
+    if (Agenda == NULL)
+    {
+        printf("\nCriando uma nova agenda.\n\n");
+    }
+    //Caso arquivo exista, extrai os dados presentes no arquivo e armazena na lista.
+    else
+    {
+        while (fgets(aux->nome, sizeof(aux->nome), Agenda) != NULL)
+        {
+            fgets(aux->telefone, sizeof(aux->telefone), Agenda);
+            fgets(aux->endereco, sizeof(aux->endereco), Agenda);
+            fscanf(Agenda, "%u\n", &aux->cep);
+            fgets(aux->nascimento, sizeof(aux->nascimento), Agenda);
+            inserirLista(&head, aux->nome, aux->telefone, aux->endereco, aux->cep, aux->nascimento);
+            //Chamada de função extra para ignorar caractere ($) no arquvio.
+            fgets(aux->nome, sizeof(aux->nome), Agenda);
+        }
+        fclose(Agenda);
+        //Organiza a lista alfabeticamente de acordo com o nome.
+        insertionSort(&head);
+    }
+    //Início do loop que define o menu do programa.
     do
     {
         printf("Funções Disponíveis:\n");
@@ -37,38 +75,55 @@ int main()
         printf("4. Visualizar registros em ordem alfabética de acordo com o nome.\n");
         printf("5. Sair.\n");
         scanf("%d", &escolha);
+        //Função utilizada para retirar "\n" do buffer de entrada.
         getchar();
         switch (escolha)
         {
         case 1:
-            inserirRegistro(&head);
+            //Struct aux recebe valores inseridos pelo usuário.
+            aux = inserirRegistro();
+            //Valores da struct aux são inseridos ao final da lista.
+            inserirLista(&head, aux->nome, aux->telefone, aux->endereco, aux->cep, aux->nascimento);
+            //A lista é organizada alfabeticamente de acordo com o nome.
+            insertionSort(&head);
             break;
-        case 2: //removerRegistrosEspecificos(); break;
+        case 2:
+            //Remove todos os registros que possuem a substring inserida.
+            removerRegistrosEspecificos(&head);
+            break;
         case 3:
-            imprimeRegistrosEspecificos();
+            //Imprime todos os registros que possuem a substring inserida.
+            imprimirRegistrosEspecificos(head);
             break;
         case 4:
-            imprimeRegistrosAlfabeticamente(head);
+            //Imprime a lista completa.
+            imprimirLista(head);
             break;
         case 5:
+            //Escreve os valores da lista no .txt;
             atualizarTxt(head);
+            //Libera a memória alocada dinamicamente.
+            freeRegistros(&head);
+            printf("\nFechando agenda.\n\n");
             break;
         default:
-            printf("Entrada inválida.\n");
+            printf("\nEntrada inválida.\n\n");
             break;
         }
     } while (escolha != 5);
     return 0;
 }
-void inserirRegistro(Contato **inicio)
+
+//Retorna uma struct com os valores inseridos pelo usuário.
+//A maior parte das operações nessa função são referentes a validação das entradas.
+struct Contato *inserirRegistro()
 {
     bool leap = false;
     int i;
-    int numeroRegistros = 1;
-    Contato *novoContato;
-    Contato *fim = *inicio;
-    novoContato = (Contato *)malloc(sizeof(Contato));
-    printf("Registre um novo contato.\nInsira o nome: ");
+    struct Contato *novoContato;
+    
+    //A entrada de nome não aceita números.
+    printf("\nRegistre um novo contato.\nInsira o nome: ");
     for (i = 0; i < strlen(novoContato->nome) - 1;)
     {
         fgets(novoContato->nome, sizeof(novoContato->nome), stdin);
@@ -82,6 +137,7 @@ void inserirRegistro(Contato **inicio)
             }
         }
     }
+    //A entrada de telefone aceita somente números e o caractere "-" na quinta posição do array.
     printf("Insira o telefone (xxxxx-xxxx): ");
     for (i = 0; i < strlen(novoContato->telefone) - 1;)
     {
@@ -104,7 +160,7 @@ void inserirRegistro(Contato **inicio)
                     continue;
                 else
                 {
-                    printf("Entrada inválida, certifique-se de digitar um número no formato xxxxx-xxxx.\n");
+                    printf("Entrada inválida, certifique-se de digitar um número no formato xxxxx-xxxx.\nDigite novamente: ");
                     break;
                 }
             }
@@ -119,11 +175,28 @@ void inserirRegistro(Contato **inicio)
             break;
         }
     }
+    //A entrada de endereço aceita qualquer string.
     printf("Insira o endereço: ");
     fgets(novoContato->endereco, sizeof(novoContato->endereco), stdin);
+    //A entrada de CEP aceita somente números.
     printf("Insira o CEP (somente dígitos): ");
-    scanf("%d", &novoContato->cep);
-    getchar();
+    char cep_aux[100];
+    while (i != strlen(cep_aux) - 1)
+    {
+        fgets(cep_aux, sizeof(cep_aux), stdin);
+        for (i = 0; i < strlen(cep_aux) - 1; i++)
+        {
+            if (!isdigit(cep_aux[i]))
+            {
+                printf("Entrada inválida, certifique-se de digitar somente números positivos.\nDigite novamente: ");
+                break;
+            }
+        }
+    }
+    novoContato->cep = atoi(cep_aux);
+    //A entrada de nascimento aceita somente números e o caractere "/" em algumas posições.
+    //A entrada será aceita e formatada somente se estiver nos formatos dd/mm/aaaa, ddmmaaaa, dd/mmaaaa ou ddmm/aaaa.
+    //Além disso, só serão aceitas datas válidas/reais.
     printf("Insira a data de nascimento no formato dd/mm/aaaa: ");
     for (i = 0; i < strlen(novoContato->nascimento) - 1;)
     {
@@ -191,9 +264,9 @@ void inserirRegistro(Contato **inicio)
         }
         int day, month, year;
         char *divide;
-        char nascimento[13];
-        strcpy(nascimento, novoContato->nascimento);
-        divide = strtok(nascimento, "/");
+        char bd[13];
+        strcpy(bd, novoContato->nascimento);
+        divide = strtok(bd, "/");
         for (temp = 0; divide != NULL; temp++)
         {
             if (temp == 0)
@@ -232,7 +305,9 @@ void inserirRegistro(Contato **inicio)
                 }
             }
             else
+            {
                 leap = true;
+            }
         }
         if (leap && month == 2 && day > 29)
         {
@@ -248,143 +323,181 @@ void inserirRegistro(Contato **inicio)
         }
         break;
     }
-    printf("Contato registrado com sucesso.\n\n\n");
-    novoContato->next = NULL;
-    if (*inicio == NULL)
-    {
-        novoContato->prev = NULL;
-        *inicio = novoContato;
-    }
-    while (fim->next != NULL)
-    {
-        fim = fim->next;
-    }
-    fim->next = novoContato;
-    novoContato->prev = fim;
-    numeroRegistros++;
-    insertionSort(&novoContato);
+    printf("\nContato registrado com sucesso.\n\n");
+    return novoContato;
 }
 
-void push(Contato **head, char nome[], char telefone[], char endereco[], unsigned int cep, char nascimento[], Contato *next, Contato *prev)
+//Organiza a lista alfabeticamente de acordo com o nome.
+//A função cria uma lista vazia e transfere os valores de forma organizada.
+void insertionSort(struct Contato **head_ref)
 {
-    Contato *newNode = (Contato *)malloc(sizeof(Contato));
+    struct Contato *sorted = NULL;
+    struct Contato *current = *head_ref;
+    while (current != NULL)
+    {
+        struct Contato *next = current->next;
+        current->prev = current->next = NULL;
+        struct Contato *aux;
+        if (sorted == NULL)
+        {
+            sorted = current;
+        }
+        else if (strcmp((sorted)->nome, current->nome) >= 0)
+        {
+            current->next = sorted;
+            current->next->prev = current;
+            sorted = current;
+        }
+        else
+        {
+            aux = sorted;
+            while (aux->next != NULL && (strcmp(aux->next->nome, current->nome) < 0))
+            {
+                aux = aux->next;
+            }
+            current->next = aux->next;
+            if (aux->next != NULL)
+            {
+                current->next->prev = current;
+            }
+            aux->next = current;
+            current->prev = aux;
+        }
+        current = next;
+    }
+    *head_ref = sorted;
+}
+
+//Libera a memória alocada dinamicamente.
+void freeRegistros(struct Contato **head)
+{
+    struct Contato *current = *head;
+    while (current != NULL)
+    {
+        struct Contato *next = current->next;
+        free(current);
+        current = next;
+    }
+}
+
+//Imprime toda a lista organizada.
+void imprimirLista(struct Contato *head)
+{
+    bool exists = false;
+    while (head != NULL)
+    {
+        if (!exists)
+        {
+            printf("\nLista de contatos:\n\n");
+            exists = true;
+        }
+        printf("Nome: %sTelefone: %sEndereço: %sCEP: %u\nData de nascimento: %s$\n", head->nome, head->telefone, head->endereco, head->cep, head->nascimento);
+        head = head->next;
+    }
+    if (!exists)
+    {
+        printf("\nLista de contatos vazia.\n\n");
+        return;
+    }
+    printf("\n");
+}
+
+//Insere um contato na lista.
+void inserirLista(struct Contato **head_ref, char nome[], char telefone[], char endereco[], unsigned int cep, char nascimento[])
+{
+    struct Contato *newNode = (struct Contato *)malloc(sizeof(struct Contato));
 
     strcpy(newNode->nome, nome);
     strcpy(newNode->telefone, telefone);
     strcpy(newNode->endereco, endereco);
     newNode->cep = cep;
     strcpy(newNode->nascimento, nascimento);
-    newNode->next = (head);
+
+    newNode->next = (*head_ref);
     newNode->prev = NULL;
 
-    if ((*head) != NULL)
-    {
-        (*head)->prev = newNode;
-    }
-    (*head) = newNode;
+    if ((*head_ref) != NULL)
+        (*head_ref)->prev = newNode;
+
+    (*head_ref) = newNode;
 }
 
-void imprimeRegistrosEspecificos()
+//Remove registros que contenham a substring fornecida.
+void removerRegistrosEspecificos(struct Contato **head)
 {
-    char stringEspecifica[100];
-    int aux = 1;
-    printf("\nEscolha a string específica:\n");
+    struct Contato *del = (*head);
+    bool exists = false;
+    char stringEspecifica[102];
+    printf("\nEscolha a string específica: ");
     scanf("%s", stringEspecifica);
-    char c[1000];
-    char linhas[1000];
-    int i = 0;
-    FILE *Agenda;
-    Agenda = fopen("contatos.txt", "r");
-    printf("Registros com a string procurada:\n");
-    while ((fgets(c, sizeof(c), Agenda)) != NULL)
+    while (del != NULL)
     {
-        if ((strstr(c, stringEspecifica)) != NULL)
+        if (strstr(del->nome, stringEspecifica) != NULL)
         {
-            printf("%s", c);
-            i = aux;
-            for (i = aux; i < aux + 5; i++)
+            if (*head == del)
             {
-                fgets(linhas, sizeof(linhas), Agenda);
-                printf("%s", linhas);
+                *head = del->next;
+            }
+            if (del->next != NULL)
+            {
+                del->next->prev = del->prev;
+            }
+            if (del->prev != NULL)
+            {
+                del->prev->next = del->next;
+            }
+            free(del);
+            if (!exists)
+            {
+                printf("\nRegistros deletados!\n\n");
+                exists = true;
             }
         }
-        aux++;
+        del = del->next;
     }
-    fclose(Agenda);
-}
-
-void insertionSort(Contato **head_ref)
-{
-    Contato *sorted = NULL;
-    Contato *current = *head_ref;
-
-    while (current != NULL)
+    if (!exists)
     {
-        Contato *next = current->next;
-        current->prev = current->next = NULL;
-        sortedInsert(&sorted, current);
-        current = next;
-    }
-    *head_ref = sorted;
-}
-
-void sortedInsert(Contato **head_ref, Contato *newNode)
-{
-    Contato *current;
-    Contato *temp;
-    Contato *temp2;
-
-    if (*head_ref == NULL)
-    {
-        *head_ref = newNode;
-    }
-    else if (strcmp((*head_ref)->nome, newNode->nome) >= 0)
-    {
-        newNode->next = *head_ref;
-        newNode->next->prev = newNode;
-        *head_ref = temp;
-    }
-    else
-    {
-        current = *head_ref;
-        temp = current->next;
-
-        while (current->next != NULL && (strcmp(current->next->nome, newNode->nome) < 0))
-        {
-            current = current->next;
-        }
-
-        newNode->next = current->next;
-
-        if (current->next != NULL)
-        {
-            newNode->next->prev = newNode;
-        }
-        current->next = temp;
-        temp->prev = current;
+        printf("\nNenhum registro encontrado com a string dada.\n\n");
+        return;
     }
 }
 
-void imprimeRegistrosAlfabeticamente(Contato *head)
+//Imprime registros que contenham a substring fornecida.
+void imprimirRegistrosEspecificos(struct Contato *head)
 {
-    Contato *fim;
+    bool exists = false;
+    char stringEspecifica[102];
+    printf("\nEscolha a string específica: ");
+    scanf("%s", stringEspecifica);
     while (head != NULL)
     {
-        printf("%s%s%s%d\n%s$\n", head->nome, head->telefone, head->endereco, head->cep, head->nascimento);
-        fim = head;
+        if (strstr(head->nome, stringEspecifica) != NULL)
+        {
+            if (!exists)
+            {
+                printf("\nRegistros com a string procurada:\n\n");
+                exists = true;
+            }
+            printf("Nome: %sTelefone: %sEndereço: %sCEP: %u\nData de nascimento: %s$\n", head->nome, head->telefone, head->endereco, head->cep, head->nascimento);
+        }
         head = head->next;
     }
+    if (!exists)
+    {
+        printf("\nNenhum registro encontrado com a string dada.\n\n");
+        return;
+    }
+    printf("\n");
 }
 
-void atualizarTxt(Contato *head)
+//Escreve os valores da lista no .txt.
+void atualizarTxt(struct Contato *head)
 {
     FILE *Agenda;
-    Contato *aux;
     Agenda = fopen("contatos.txt", "w+");
     while (head != NULL)
     {
-        aux = head;
-        fprintf(Agenda, "%s%s%s%d\n%s$\n", aux->nome, aux->telefone, aux->endereco, aux->cep, aux->nascimento);
+        fprintf(Agenda, "%s%s%s%u\n%s$\n", head->nome, head->telefone, head->endereco, head->cep, head->nascimento);
         head = head->next;
     }
     fclose(Agenda);
