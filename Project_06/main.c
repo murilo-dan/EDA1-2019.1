62d8047ff90a0841ada4deb00a7e246786826508
Gabriel Alves Hussein <gabrielhussein83@gmail.com>
2019-06-26 00:07:10 -0300

    Implementação do algoritmo de back propagation, faltando realizar a derivada e aplicar épocas.

--- Project_06/main.c
+++ Project_06/main.c
@@ -12,7 +12,8 @@ Gabriel Alves Hussein - 17/0103200
 #include <math.h>
 #include <stdbool.h>
 
-double *shufflePesos(double *);
+double *shufflePesos(double *, int, int);
+double *shuffleBias(double *, int);
 int *shuffleTraining(int *);
 //Declaração do protótipo das funções utilizadas.
 
@@ -27,64 +28,67 @@ int main(int argc, char *argv[])
   FILE *fp;
   double asphaltTraining[25];
   double grassTraining[25];
-  double erro;
+  double erro = 0;
+  double gradienteEntrada[536], gradienteOculta[input], gradienteSaida = 0;
+  double learningRate;
   int random[25] = {0};
-  double b;
-  double pesos[536];
+  double camadaSaida = 0.0;
+  double bEntrada[536], bOculta[input], bSaida;
+  double pesosEntrada[287296], pesosOculta[input*2], pesosSaida[input];
   shuffleTraining(random);
   fp = fopen("features.txt", "r");
   int j = 0, aux = 0;
-  double saidaAsphalt[536], saidaGrass[536], n, euler = 2.718281828459045235360287;
+  double saidaAsphalt[536], saidaAsphaltOculta[input], saidaGrass[536], n, euler = 2.718281828459045235360287;
   for(int i = 0; i<25; i++){
     printf("%d\n",random[i]);
   }
   while(aux<25)
   {
-    if(j == random[aux])
+    if(j == random[aux] && aux==0)
     {
       double* camadaEntrada = calloc(536, sizeof(double));
       for(int a = 0; a < 536; a++)
       {
         fscanf(fp, "%lf", &*(camadaEntrada+a));
       }
+      shuffleBias(bEntrada, 536);
       for(int i = 0; i<536; i++)
       {
-        shufflePesos(pesos);
-        b = rand()%15000;
+      shufflePesos(pesosEntrada, i+536, 536);
         for(int a = 0; a<536; a++)
         {
-          n += *(camadaEntrada+a) * pesos[a];
+          n += *(camadaEntrada+a) * pesosEntrada[a];
         }
-        n += b;
+        n += bEntrada[i];
         saidaAsphalt[i] = (1/(1+(pow(euler, -n))));
         n = 0;
       }
-      double camadaOculta[input];
+      shuffleBias(bOculta, input);
       for(int a = 0; a<input; a++)
       {
-        shufflePesos(pesos);
-        b = rand()%15000;
+        shufflePesos(pesosOculta, a+input, input);
         for(int k = 0; k<536; k++)
         {
-          camadaOculta[a] += saidaAsphalt[k] * pesos[a];
+          n += saidaAsphalt[k] * pesosOculta[k];
         }
-        camadaOculta[a] += b;
-        camadaOculta[a] = (1/(1+(pow(euler, -camadaOculta[a]))));
+        n += bOculta[a];
+        saidaAsphaltOculta[a] = (1/(1+(pow(euler, -n))));
+        n = 0;
       }
-      double camadaSaida = 0.0;
-      shufflePesos(pesos);
-      b = rand()%15000;
+      for(int i = 0;i < input;i++){
+        pesosSaida[i] = rand()%15000;
+      }
+      bSaida = rand()%15000;
       for(int a = 0; a<input; a++)
       {
-        camadaSaida += camadaOculta[a] * pesos[a];
+        camadaSaida += saidaAsphaltOculta[a] * pesosSaida[a];
       }
-      camadaSaida += b;
+      camadaSaida += bSaida;
       camadaSaida = (1/(1+(pow(euler, -camadaSaida))));
       asphaltTraining[aux] = camadaSaida;
-      printf("%lf\n",asphaltTraining[aux]);
       erro = 0 - camadaSaida;
       //lendo 50 vezes para chegar no equivalente de grama
-      for(int i = 1; i<50; i++)
+      /*for(int i = 1; i<50; i++)
       {
         double* temp = calloc(536, sizeof(double));
         for(int a = 0; a < 536; a++)
@@ -92,7 +96,7 @@ int main(int argc, char *argv[])
           fscanf(fp, "%lf", &*(temp+a));
         }
         free(temp);
-      }/*
+      }
       for(int a = 0; a < 536; a++)
       {
         fscanf(fp, "%lf", &*(camadaEntrada+a));
@@ -133,6 +137,78 @@ int main(int argc, char *argv[])
       aux++;
       free(camadaEntrada);
     }
+    else if(aux!=0)
+    {
+      for(int i=0; i<536; i++)
+      {
+        gradienteEntrada[i];
+      }
+      for(int i=0; i<input;i++)
+      {
+        gradienteOculta[i];
+      }
+      gradienteSaida = /*derivada de*/ camadaSaida * erro;
+      for(int i=0; i<536; i++)
+      {
+        for(int j=0; j<536; j++)
+        {
+          pesosEntrada[j+i] = (pesosEntrada[j+i] + learningRate) * camadaSaida * *gradienteEntrada;
+        }
+        bEntrada[i] = (bEntrada[i] + learningRate) * *gradienteEntrada;
+      }
+      for(int i=0; i<input; i++)
+      {
+        for(int j=0; j<input; j++)
+        {
+          pesosOculta[j+i] = (pesosOculta[j+i] + learningRate) * saidaAsphalt[j] * *gradienteOculta;
+        }
+        bOculta[i] = (bOculta[i] + learningRate) * *gradienteOculta;
+      }
+      for(int i=0; i<input; i++)
+      {
+        pesosSaida[i] = (pesosSaida[i] + learningRate) * saidaAsphaltOculta[j] * gradienteSaida;
+        bSaida = (bSaida + learningRate) * gradienteSaida;
+      }
+      double* camadaEntrada = calloc(536, sizeof(double));
+      for(int a = 0; a < 536; a++)
+      {
+        fscanf(fp, "%lf", &*(camadaEntrada+a));
+      }
+      for(int i = 0; i<536; i++)
+      {
+        for(int a = 0; a<536; a++)
+        {
+          n += *(camadaEntrada+a) * pesosEntrada[a];
+        }
+        n += bEntrada[i];
+        saidaAsphalt[i] = (1/(1+(pow(euler, -n))));
+        n = 0;
+      }
+      for(int a = 0; a<input; a++)
+      {
+        shufflePesos(pesosOculta, a+input, input);
+        for(int k = 0; k<536; k++)
+        {
+          n += saidaAsphalt[k] * pesosOculta[k];
+        }
+        n += bOculta[a];
+        saidaAsphaltOculta[a] = (1/(1+(pow(euler, -n))));
+        n = 0;
+      }
+      for(int i = 0;i < input;i++){
+        pesosSaida[i] = rand()%15000;
+      }
+      bSaida = rand()%15000;
+      for(int a = 0; a<input; a++)
+      {
+        camadaSaida += saidaAsphaltOculta[a] * pesosSaida[a];
+      }
+      camadaSaida += bSaida;
+      camadaSaida = (1/(1+(pow(euler, -camadaSaida))));
+      asphaltTraining[aux] = camadaSaida;
+      erro = 0 - camadaSaida;
+      aux++;
+    }
     else
     {
       double* temp = calloc(536, sizeof(double));
@@ -149,11 +225,18 @@ int main(int argc, char *argv[])
   return 0;
 }
 
-double *shufflePesos(double *pesos){
-  for(int i = 0;i < 536;i++){
-    pesos[i] = rand()%15000;
+double *shufflePesos(double *pesos, int current, int limit){
+  for(int i = 0;i < limit;i++){
+    pesos[current+i] = rand()%15000;
   }
   return pesos;
+}
+
+double *shuffleBias(double *bias, int limit){
+  for(int i = 0;i < limit;i++){
+    bias[i] = rand()%15000;
+  }
+  return bias;
 }
 
 int *shuffleTraining(int *vetor){
